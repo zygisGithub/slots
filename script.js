638b333bbb7ec007b44d01c9cc454f5d2d39f8da
dzigi <zygisbuzas@gmail.com>
2024-05-15 15:05:15 +0300

    finalUpdate

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ const spin = document.getElementById('spin')
 const moneyDisplay = document.getElementById('moneyDisplay')
 const bet = document.getElementById('bet')
 const win = document.getElementById('win')
+const jackpotSound  = document.getElementById('jackpotSound')
+const winSound = document.getElementById('winSound')
+const spinSound = document.getElementById('spinSound')
 let spinning = false
 let money = 1000
 
 spin.onclick = function () {
     if (!spinning && bet.value <= money && bet.value >= 1){
+        spinSound.currentTime = 1
+        spinSound.play()
         money -= bet.value
         moneyUpdate()
         spinReels()
@@ -66,72 +71,82 @@ function spinReels() {
     // Add your win conditions based on the images
     if (reel1Image === 'cherry' && reel2Image === 'cherry' && reel3Image === 'cherry') {
         setTimeout(function () {
-            winDisplay(`Mega win: ${bet.value*200}$`)
+            winDisplay('Mega Win', `${bet.value*200}`)
             money += bet.value*200
             moneyUpdate()
+            playJackpotSound()
         }, 1100)
     }
     if (reel1Image === 'seven' && reel2Image === 'seven' && reel3Image === 'seven') {
         setTimeout(function () {
-            winDisplay(`Big win: ${bet.value*20}$`)
+            winDisplay('Big Win', `${bet.value*20}`)
             money += bet.value*20
             moneyUpdate()
+            playWinSound()
         }, 1100)
     }
     if (reel1Image === 'banana' && reel2Image === 'banana' && reel3Image === 'banana') {
         setTimeout(function () {
-            winDisplay(`Huge win: ${bet.value*50}$`)
+            winDisplay('Huge Win', `${bet.value*50}`)
             money += bet.value*50
             moneyUpdate()
+            playJackpotSound()
         }, 1100)
     }
     if (reel1Image === 'watermelon' && reel2Image === 'watermelon' && reel3Image === 'watermelon') {
         setTimeout(function () {
-            winDisplay(`Big win: ${bet.value*20}$`)
+            winDisplay('Big Win', `${bet.value*20}`)
             money += bet.value*20
             moneyUpdate()
+            playWinSound()
         }, 1100)
     }
     if (reel1Image === 'lemon' && reel2Image === 'lemon' && reel3Image === 'lemon') {
         setTimeout(function () {
-            winDisplay(`Epic win: ${bet.value*100}$`)
+            winDisplay('Epic Win', `${bet.value*100}`)
             money += bet.value*100
             moneyUpdate()
+            playJackpotSound()
         }, 1100)
     }
     if (reel1Image === 'bar' && reel2Image === 'bar' && reel3Image === 'bar') {
         setTimeout(function () {
-            winDisplay(`Huge win: ${bet.value*50}$`)
+            winDisplay('Huge Win', `${bet.value*50}`)
             money += bet.value*50
             moneyUpdate()
+            playJackpotSound()
         }, 1100)
     }
     if (reel1Image === 'bell' && reel2Image === 'bell' && reel3Image === 'bell') {
         setTimeout(function () {
-            winDisplay(`Huge win: ${bet.value*50}$`)
+            winDisplay('Huge Win', `${bet.value*50}`)
             money += bet.value*50
             moneyUpdate()
+            playJackpotSound()
         }, 1100)
     }
     if (reel1Image === 'orange' && reel2Image === 'orange' && reel3Image === 'orange') {
         setTimeout(function () {
-            winDisplay(`Big win: ${bet.value*20}$`)
+            winDisplay('Big Win', `${bet.value*20}`)
             money += bet.value*20
             moneyUpdate()
+            playWinSound()
         }, 1100)
     }
     if (reel1Image === 'plum' && reel2Image === 'plum' && reel3Image === 'plum') {
         setTimeout(function () {
-            winDisplay(`Big win: ${bet.value*20}$`)
+            winDisplay('Big Win', `${bet.value*20}`)
             money += bet.value*20
             moneyUpdate()
+            playWinSound()
         }, 1100)
     }
     if (reel1Image === reel2Image || reel2Image === reel3Image) {
         setTimeout(function () {
             money += bet.value*2
-            winDisplay(`You win: ${bet.value*2}$`)
+            winDisplay('You Win', `${bet.value*2}`)
             moneyUpdate()
+            playWinSound()
         }, 1100)
     }
 }
@@ -146,12 +161,20 @@ function moneyUpdate () {
     moneyDisplay.innerHTML = `Your Money: ${money}$`
 }
 
-function winDisplay (winCon) {
+function winDisplay (winCon, amount) {
     win.style.display = 'block'
     win.style.animation = 'win 1s linear'
-    win.innerHTML = `${winCon}`
+    win.innerHTML = `<p>${winCon}</p>
+                        <p>${amount}$</p>`
     setTimeout(function () {
         win.style.display = 'none'
         win.style.animation = ''
     },1000)
+}
+
+function playWinSound () {
+    winSound.play()
+}
+function playJackpotSound () {
+    jackpotSound.play()
 }
